9419e4d31e6f253f3cf589190b42abd9aa54cc26
Logan Tom <tom.lo@northeastern.edu>
2026-03-29 22:49:02 -0400

    refactored video processing, implemented rudamentary data storage to JSON

--- main.py
+++ main.py
@@ -1,185 +1,58 @@
-import numpy as np
-import mediapipe as mp
-from mediapipe.tasks import python
-from mediapipe.tasks.python import vision
-from player_data import PlayerData
 from metric_extractor import MetricExtractor
-import cv2
-import math
-SCALE = .4
+from video_processor import VideoProcessor
+import json
+USER = "Logan"
+VIDEO_PATH = "media\hit2 - Trim.mp4"
+GENDER = "Male"
+VIDEO_DATE = "01-17-2025"
+try: 
+    with open("data.json", "r") as json_file:
+        loaded_data = json.load(json_file)
+        if(USER not in loaded_data):
+            loaded_data[USER] = {"sessions": []}
+except FileNotFoundError:
+    loaded_data = {}
+    loaded_data[USER] = {"sessions": []}
+user_data = loaded_data[USER]
 
-debug = True
-padding = 5
-# 0 - nose
-# 1 - left eye (inner)
-# 2 - left eye
-# 3 - left eye (outer)
-# 4 - right eye (inner)
-# 5 - right eye
-# 6 - right eye (outer)
-# 7 - left ear
-# 8 - right ear
-# 9 - mouth (left)
-# 10 - mouth (right)
-# 11 - left shoulder
-# 12 - right shoulder
-# 13 - left elbow
-# 14 - right elbow
-# 15 - left wrist
-# 16 - right wrist
-# 17 - left pinky
-# 18 - right pinky
-# 19 - left index
-# 20 - right index
-# 21 - left thumb
-# 22 - right thumb
-# 23 - left hip
-# 24 - right hip
-# 25 - left knee
-# 26 - right knee
-# 27 - left ankle
-# 28 - right ankle
-# 29 - left heel
-# 30 - right heel
-# 31 - left foot index
-# 32 - right foot index
-POSE_CONNECTIONS = [
-    (0,1),(1,2),(2,3),(3,7),(0,4),(4,5),(5,6),(6,8),
-    (9,10),(11,12),(11,13),(13,15),(15,17),(15,19),(15,21),(17,19),
-    (12,14),(14,16),(16,18),(16,20),(16,22),(18,20),
-    (11,23),(12,24),(23,24),(23,25),(24,26),(25,27),(26,28),
-    (27,29),(28,30),(29,31),(30,32),(27,31),(28,32)
-]
 
-base_options = python.BaseOptions(model_asset_path='pose_landmarker_heavy.task')
-options = vision.PoseLandmarkerOptions(
-    base_options=base_options,
-    output_segmentation_masks=False,
-    num_poses = 1
-)
-landmarker = vision.PoseLandmarker.create_from_options(options)
-# Open video and grab first frame for ROI selection
-cap = cv2.VideoCapture("media\hit2 - Trim.mp4")
-fps = cap.get(cv2.CAP_PROP_FPS)
-ret, first_frame = cap.read()
-h, w = first_frame.shape[:2]
-# Let user draw a box around the hitter
-display_first = cv2.resize(first_frame, (int(w * SCALE), (int (h * SCALE))))
-roi = cv2.selectROI("Select the hitter", display_first, fromCenter=False)
-cv2.destroyAllWindows()
-rx, ry, rw, rh = [int (v/SCALE) for v in roi]
-roi_cx = rx + rw / 2
-roi_cy = ry + rh / 2
-max_drift = rw * .6
-box_h = rh + padding
-box_w = rw + padding
+video_processor = VideoProcessor(VIDEO_PATH, USER)
 
-def closest_pose_to_roi(pose_landmarks_list, w, h, roi_cx, roi_cy):
-    best_idx = 0
-    best_dist = float('inf')
-    for i, landmarks in enumerate(pose_landmarks_list):
-        # Use hip midpoint as person center
-        lshoulder = landmarks[11]
-        rshoulder = landmarks[12]
-        lhip = landmarks[23]
-        rhip = landmarks[24]
-        
-        cx = ((lhip.x + rhip.x + lshoulder.x + rshoulder.x) / 4) * w
-        cy = ((lhip.y + rhip.y + lshoulder.y + rshoulder.y) / 4) * h
-        dist = (cx - roi_cx) ** 2 + (cy - roi_cy) ** 2
-        if dist < best_dist:
-            best_dist = dist
-            best_idx = i
-    return best_idx
-
-paused = False
-frame_num = 0
-hitter = PlayerData(fps)
-contact_frame = -100
-while cap.isOpened():
-    if not paused:
-        ret, frame = cap.read()
-        if not ret:
-            break
-        process_frame = True
-    else:
-        process_frame = False
-
-    key = cv2.waitKey(1) & 0xFF
-    if key == ord('q'):
-        break
-    elif key == ord('p'):
-        paused = not paused
-    elif key == ord('.') and paused:
-        ret, frame = cap.read()
-        if not ret:
-            break
-        process_frame = True
-    elif key == ord('d'):
-        print(frame_num)
-    elif key == ord('c'):
-        contact_frame = frame_num
-        print(f"Contact tagged at frame {frame_num}")
-
-    if process_frame:
-        frame_num = frame_num + 1
-        h, w = frame.shape[:2]
-        y_start = max(0, int(roi_cy - box_h/2))
-        y_end = min(h, int(roi_cy + box_h/2))
-        x_start = max(0, int(roi_cx - box_w/2))
-        x_end = min(w, int(roi_cx + box_w/2))
-        cropped = frame[y_start:y_end, x_start:x_end]   
-        rgb = cv2.cvtColor(cropped, cv2.COLOR_BGR2RGB)
-        mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=rgb)
-        result = landmarker.detect(mp_image)
-        
-        
-        if result.pose_landmarks:
-
-            print(f"Frame {frame_num}:")
-            idx = closest_pose_to_roi(result.pose_landmarks, x_end - x_start, y_end - y_start, (x_end - x_start) / 2, (y_end - y_start) / 2)
-            landmarks = result.pose_landmarks[idx]
-            crop_w = x_end - x_start
-            crop_h = y_end - y_start
-            hitter.update(frame_num, landmarks, crop_w, crop_h)
-
-            for lm in landmarks:
-                # translate from crop space to full frame space
-                full_x = int(x_start + lm.x * (x_end - x_start))
-                full_y = int(y_start + lm.y * (y_end - y_start))
-                cv2.circle(frame, (full_x, full_y), 4, (0, 0, 255), -1)
-            
-            for a, b in POSE_CONNECTIONS:
-                a_x = int(x_start + landmarks[a].x * (x_end - x_start))
-                a_y = int(y_start + landmarks[a].y * (y_end - y_start))
-                b_x = int(x_start + landmarks[b].x * (x_end - x_start))
-                b_y = int(y_start + landmarks[b].y * (y_end - y_start))
-                cv2.line(frame,(a_x,a_y), (b_x,b_y), (0, 255, 0),3)
-                    
-
-            lshoulder, rshoulder = landmarks[11], landmarks[12]
-            lhip, rhip = landmarks[23], landmarks[24]
-
-            new_cx = x_start + ((lshoulder.x + rshoulder.x + lhip.x + rhip.x) / 4) * (x_end - x_start)
-            new_cy = y_start + ((lshoulder.y + rshoulder.y + lhip.y + rhip.y) / 4) * (y_end - y_start)
-
-            dist = ((new_cx - roi_cx)**2 + (new_cy - roi_cy)**2) ** 0.5
-            if dist < max_drift:
-                roi_cx = new_cx
-                roi_cy = new_cy
-
-            if debug:
-                cv2.circle(frame, (int(roi_cx), int(roi_cy)), 10, (255, 0, 0), -1)
-                cv2.rectangle(frame, (x_start, y_start), (x_end, y_end), (255, 0, 0), 2)
-
-        display = cv2.resize(frame, (int(w * SCALE), int(h * SCALE)))
-        cv2.imshow("Pose", display)
-
+video_processor.process()
 
 #GRAPHING
-extractor = MetricExtractor(hitter, fps = fps, contact_frame= contact_frame)
+extractor = MetricExtractor(video_processor.hitter, fps = video_processor.fps, contact_frame= video_processor.contact_frame)
 extractor.graph_velocities()
-max_shoulder_velocity, peak_timing_ms_before_contact, onset_ms_before_contact, hip_shoulder_max_diff = extractor.get_metrics()
+max_shoulder_velocity, peak_rotation_velocity_before_contact, shoulder_onset_before_contact, hip_shoulder_max_diff = extractor.get_metrics()
+
+#Store metrics to User
+session = {
+    "video": VIDEO_PATH,
+    "date": VIDEO_DATE,
+    "gender": GENDER,
+    "metrics": {
+        "peak_trunk_velocity": max_shoulder_velocity,
+        "peak_timing_ms_before_contact": peak_rotation_velocity_before_contact,
+        "onset_ms_before_contact": shoulder_onset_before_contact,
+        "hip_shoulder_peak_diff_ms": hip_shoulder_max_diff
+    }
+}
+existing = user_data["sessions"]
+is_duplicate = any(s["video"] == VIDEO_PATH and s["date"] == VIDEO_DATE for s in existing)
+if(not is_duplicate):
+    user_data["sessions"].append(session)
+print("\nExtracted Metrics:")
+metrics = session["metrics"]
+print(f"  Peak trunk velocity: {metrics['peak_trunk_velocity']:.1f} °/s")
+print(f"  Peak timing before contact: {metrics['peak_timing_ms_before_contact']:.1f} ms")
+print(f"  Onset before contact: {metrics['onset_ms_before_contact']:.1f} ms")
+print(f"  Hip-shoulder peak diff: {metrics['hip_shoulder_peak_diff_ms']:.1f} ms")
+
+save = input("\nSave this session? (y/n): ")
+if save.lower() == 'y':
+    # save to json
+    with open("data.json", "w") as json_file:
+        json.dump(loaded_data, json_file, indent=2)
 #ANALYSIS
 BENCHMARKS = {
     "male": {
